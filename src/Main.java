277e4c62b0893553b08a6f245007766d145f77a5
Vasiliy Alferov <ya-ikmik2012@yandex.ru>
2017-09-10 01:10:47 +0300

    Hw 01 is ready

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,6 @@ import ru.spbau.alferov.javahw.hashtable.*;
 public class Main {
 
     public static void main(String[] args) {
-        /*
-	    List l = new List();
-
-	    for (int i = 1; i <= 17; i++) {
-	        l.insert(i);
-        }
-
-        for (List.Iterator it = l.listHead(); !it.isEnd(); it.advance()) {
-            if ((Integer)it.get() == 7) {
-                l.erase(it);
-                break;
-            }
-        }
-
-        l.erase(l.listHead());
-
-        System.out.print(l.getSize());
-        System.out.print(": ");
-	    for (List.Iterator it = l.listHead(); !it.isEnd(); it.advance()) {
-	        Integer cur = (Integer)it.get();
-            System.out.print(cur);
-            System.out.print(" ");
-        }
-        System.out.println();
-        */
-
-        Hashtable h = new Hashtable();
-
-        h.put("a", "b");
-        System.out.println(h.get("a"));
-        System.out.println(h.put("a", "c"));
-        System.out.println(h.get("a"));
-        h.clear();
+        
     }
 }
