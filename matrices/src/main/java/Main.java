aba7a68a6c3b8d1518fcfdc23b90e1ec23c1e02b
Vasily Alferov <ya-ikmik2012@yandex.ru>
2017-09-27 12:08:13 +0300

    Remove main body (Matrix hw is ready).

--- matrices/src/main/java/Main.java
+++ matrices/src/main/java/Main.java
@@ -2,17 +2,6 @@ import ru.spbau.alferov.javahw.matrices.Matrix;
 
 public class Main {
     public static void main(String[] argv) {
-        int[][] cur = {{3,2,1},{4,9,8},{5,6,7}};
-        Matrix m = new Matrix(cur);
-        m.sortColumns();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(m.getElement(i, j));
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
 
-        m.printSpiralBypass();
     }
 }
